a39db82a547fca19c79f2e6a29478c24c6f6e287
Shivani-zyz <shivanikoushal2233@gmail.com>
2025-06-15 08:58:59 +0530

    Add files via upload

---
+++ services/Service.jsx
@@ -0,0 +1,308 @@
+import './Service.css'
+
+function Service(){
+
+
+    return(
+        <>
+        
+
+      <div className='robot'>
+        <img src="./src/assets/components/services/robot.jpeg" alt="" />
+
+        <div className='robot1'>
+          <h1>
+            Unlock Values<br/>
+            with Digitization & AI</h1><br />
+            <p>AI integration into the open field crop cultivation process promises a future of enhanced efficiency, sustainability, and productivity. At AgriPilot.ai, leveraging advanced AI-driven technologies, the platform meets your organization’s digitization needs across the agri-value chain, from whole farm management to AI-recommendations to traceability. </p>
+        </div>
+      </div>
+
+
+<div className='green'>
+
+  <h2 style={{color:"#00c274"}}>From Soil Test to Crop Selection to Production</h2><br />
+  <h1>Explore How AI is Making a Splash in<br/> the World of Open Field?</h1><br />
+  <p>With you at every stage, AgriPilot.ai helps you achieve precision farming, monitoring crops, optimizing resources, and boosting productivity, let's explore:</p><br /><br /><br />
+
+
+      <div className='explore'>
+        <div className='explore1'>
+          <p>Farm & Crop Assessment</p><br />
+          <span> Leveraging AI-driven technologies, AgriPilot.ai enables farmland mapping & crop assessment for informed decision making.</span><br /><br />
+          <ul>
+            <li>Capture data from satellite imagery for accurate field insights</li>
+            <li>Collect real-time data through IoT sensors for continuous farm monitoring</li>
+            <li>Precise farmland mapping to optimize resource allocation</li>
+            <li>Precise farmland mapping to optimize resource allocation</li>
+          </ul>
+        </div>
+
+        <img src="./src/assets/components/services/land.svg" alt="" />
+      </div>
+
+      
+
+      <div className="explore">
+        <img src="./src/assets/components/services/soil.svg" alt="" />
+
+        <div className='explore1'>
+          <p>Soil and Water Testing & Analysis</p><br />
+          <span>AI-driven optimal location workflow and Soil Pilot powered by AgriPilot.ai helps you with precise soil and water nutrient analysis.</span><br /><br />
+          <ul>
+            <li>Identify optimal sites for IoT sensors and soil sampling</li>
+            <li>Integrate IoTs to collect real-time sensor-based data</li>
+            <li>Detect nutrient imbalances and assess their impact on crop yieldv</li>
+            <li>Monitor soil condition accurately through computer vision models</li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div className="explore">
+
+        <div className='explore1'>
+          <p>Irrigation and Fertilization</p><br />
+          <span>AgriPilot.ai optimizes irrigation through AI-driven automation and enables early detection of pests, diseases, or nutrient deficiencies.</span><br /><br />
+          <ul>
+            <li>Monitor soil moisture and obtain water indices for proper water allocation</li>
+            <li>Detects and addresses leaks swiftly, preventing water waste and crop damage</li>
+            <li>Identify mold, rot, insects, and other threats to crop health</li>
+            <li>Schedule fertilization and pest control to minimize pesticide use </li>
+          </ul>
+        </div>
+
+
+        <img src="./src/assets/components/services/Irrigation.svg" alt="" />
+      </div>
+
+
+
+
+      <div className="explore">
+        <img src="./src/assets/components/services/AI-driven.svg" alt="" />
+
+        <div className='explore1'>
+          <p>AI-driven Weather
+Predictions</p><br />
+          <span>AgriPilot.ai leverages AI-driven solutions to predict extreme weather patterns, significantly reducing crop loss.</span><br /><br />
+          <ul>
+            <li>Satellite imagery and APIs provides precise farm-specific weather predictions</li>
+            <li>Optimize irrigation and ensure crop hydration with accurate forecasting</li>
+            <li>Schedule planting to maximize yield and minimize weather risks</li>
+            <li>Predictive insights help time harvests before adverse weather</li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div className="explore">
+
+        <div className='explore1'>
+          <p>Yield Mapping and
+Predictive Analytics</p><br />
+          <span>Yield Mapping and
+Predictive Analytics</span><br /><br />
+          <ul>
+            <li>Understand crop patterns and characteristics for better planning</li>
+            <li>Predict soil yield accurately using satellite, sensor, and drone data</li>
+            <li>Precisely forecast future yields for targeted crops</li>
+            <li>Identify the best locations and timing for sowing to maximize crop yield</li>
+            <li>Allocate resources efficiently for the highest ROI</li>
+          </ul>
+        </div>
+
+
+        <img src="./src/assets/components/services/Yield.svg" alt="" />
+      </div>
+
+
+
+
+
+    <div className="explore">
+        <img src="./src/assets/components/services/Crop.svg" alt="" />
+
+        <div className='explore1'>
+          <p>Crop Planning and
+Monitoring</p><br />
+          <span>Enables advanced planning and monitoring with AI-driven insights to optimize crop performance.</span><br /><br />
+          <ul>
+            <li>AI automation for water conservation and optimized irrigation</li>
+            <li>AGI-based recommendations for seasonal crop selection based on soil reports</li>
+            <li>Guidance on crop lifecycle from selection to post-harvest</li>
+            <li>Remote sensing technology for farm monitoring</li>
+            <li>Early problem detection, crop growth indicators, and yield forecasting</li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div className="explore">
+
+        <div className='explore1'>
+          <p>Intelligent Pesticides,
+Weeding & Harvesting</p><br />
+          <span>AgriPilot.ai revolutionizes pest control, weeding & harvesting through advanced technologies such as AI-driven drones, rovers etc</span><br /><br />
+          <ul>
+            <li>Utilize AI-powered drones for accurate pesticide application, minimizing waste</li>
+            <li>Employ AI-driven systems to distinguish weeds from crops by analyzing leaf size, shape, and color</li>
+            <li>Autonomous rovers to detect and manage weeds, reducing the need for herbicides and manual labor</li>
+            <li>Intelligent scheduling and real-time monitoring optimize the harvesting process</li>
+          </ul>
+        </div>
+
+
+        <img src="./src/assets/components/services/Weeding.svg" alt="" />
+      </div>
+
+
+
+
+      <div className="explore">
+        <img src="./src/assets/components/services/Marketplace.svg" alt="" />
+
+        <div className='explore1'>
+          <p>Marketplace & Supply
+Chain Management</p><br />
+          <span>AgriPilot.ai streamlines buyer seller connectivity, supply chain management to traceability with advanced AI techniques.
+
+</span><br /><br />
+          <ul>
+            <li>Intelligent scheduling and real-time monitoring reduce post-harvest loss</li>
+            <li>Supports farmers in meeting safety regulations and maintaining crop quality</li>
+            <li>Connects buyers and suppliers for streamlined buying selling process</li>
+            <li>AI analyses transportation routes, weather, and schedules for cost-effective movement of products</li>
+            <li>Track the origin of the food to confirm whether it's organic or not.</li>
+          </ul>
+        </div>
+      </div>
+
+
+</div>
+
+
+
+
+
+<div className="robot">
+  <div className='robo1'>
+  <h1>Controlled Environment Agriculture </h1><br />
+  <p>Controlled Environment Agriculture is a method of growing crops known for its sustainable practices in reducing land usage, water, pesticides, and other factors that often impact outdoor crops. At AgriPilot.ai, we transform closed controlled farming, such as Polyhouse, Aeroponics, Hydroponic, Aquaponic etc. with cutting-edge IoT and AI-driven solutions. Our advanced technology suite empowers farmers to optimize every aspect of their closed farming operations.</p>
+
+</div>
+</div>
+
+<div className='poly'>
+  <img src="./src/assets/components/services/polyhouse.svg" alt="" />
+
+  <div className="poly1">
+    <h1>Why Polyhouse Farming?</h1><br />
+    <p>Polyhouse enables the cultivation of a wide range of crops with enhanced yield, quality, and resource conservation by providing a stable and carefully controlled environment. With the ability to control factors such as temperature, humidity, and light, many farmers and growers are turning to Polyhouse farming as a solution for adapting to climate change, conserving resources, and ensuring food security.</p>
+  </div>
+
+</div>
+
+
+<div className="green">
+
+  <h1>Key Aspects to Consider</h1>
+
+<div className="explore">
+  <div className="explore1">
+    <p>Crop Selection and
+Cultivation Techniques</p><br />
+    <ul>
+      <li>Crop Selection and
+Cultivation Techniques</li>
+      <li>Recommendations on appropriate cultivation techniques such as soil-based farming, hydroponics, aeroponics, or aquaponics. </li>
+    </ul>
+  </div>
+
+  <img src="./src/assets/components/services/Selection.svg" alt="" />
+
+</div>
+
+
+
+
+<div className="explore">
+   <img src="./src/assets/components/services/Management.svg" alt="" />
+
+  <div className="explore1">
+    <p>Irrigation and Nutrient
+Management</p><br />
+    <ul>
+      <li>Irrigation and Nutrient
+Management</li>
+      <li>Employs advanced AI-driven irrigation techniques such as fogging, dripping, and flow table methods to enhance water use efficiency.</li>
+      <li>Automate water flow control to ensure precise irrigation.</li>
+    </ul>
+  </div>
+
+</div>
+
+
+<div className="explore">
+  <div className="explore1">
+    <p>Monitoring & Maintenance</p><br />
+    <ul>
+      <li>Monitor light intensity through light sensors to provide the ideal conditions for crop development.</li>
+      <li>Track real-time humidity and temperature data through integrated sensors, displayed via an LCD connection to maintain optimal growing conditions.</li>
+      <li>Address any potential issues before they escalate.</li>
+      <li>Inspect your crops for signs of pest infestations, diseases and nutrient deficiencies.</li>
+    </ul>
+  </div>
+
+  <img src="./src/assets/components/services/Maintenance.svg" alt="" />
+
+</div>
+
+
+<div className="explore">
+   <img src="./src/assets/components/services/Chain.svg" alt="" />
+
+  <div className="explore1">
+    <p>Harvesting and
+Supply Chain</p><br />
+    <ul>
+      <li>AI algorithms to identify when crops reach maturity, ensuring they are ready for harvest.</li>
+      <li>Autonomous harvesting and post-harvest handling practices minimize waste.
+</li>
+      <li>Marketplace connects buyers and suppliers, streamlining the buying and selling process.</li>
+      <li>AI analyses transportation routes, weather, and schedules for cost-effective product movement.</li>
+      <li>Track the origin of food to verify its organic status.
+</li>
+    </ul>
+  </div>
+
+</div>
+
+</div>
+
+  
+        
+ 
+
+
+
+
+
+<div className='look'>
+  <div className='look1'>
+    <h1>Looking for ways to<br/>implement AI in your<br/>farming operations?</h1><br />
+    <p>Let’s discuss. Get in touch with our agricultural experts at<br/>
+AgriPilot.ai and take the next step towards a sustainable future.</p>
+  </div>
+  
+
+  <img src="./src/assets/components/services/Look.svg" alt="" />
+</div>
+      
+       </>
+    )
+}
+export default Service
